591aa318ae6cf759690dd30039bf6bfb69707482
H-Neethika <neethikahariharasakthy2000@gmail.com>
2025-08-15 23:39:41 +0530

    Payment service application.yaml  and websecurity updated with the internal scope

--- PaymentService/src/main/java/com/cloud/PaymentService/security/WebSecurityConfig.java
+++ PaymentService/src/main/java/com/cloud/PaymentService/security/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig {
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
                                 .antMatchers("/payment/**")
-                                .hasAuthority("SCOPE_internal")
+                                .hasAuthority("SCOPE_internal","SCOPE_email")
                                 .anyRequest()
                                 .authenticated()
                                 )
